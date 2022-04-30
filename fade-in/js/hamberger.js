1f7e13110901b06813a32ae44287ea4ea7dc8b11
macmeals <loseworld9731@gmail.com>
2022-04-30 14:04:47 +0900

    Initial commit

---
+++ fade-in/js/hamberger.js
@@ -0,0 +1,47 @@
+jQuery( function( $ ) {
+	$( ".p-hamberger" ).on( "click", function() {
+			$( ".c-btn__cross" ).toggleClass( "is-opencross" );
+			$( ".c-hamburger__header" ).toggleClass( "is-open" );
+			$( ".c-hamburger__background" ).toggleClass( "is-open" );
+			$( ".l-side" ).toggleClass( "is-open" );
+	} );
+
+
+
+	
+		function fadeAnime(){
+			$('.c-fadeTriger').each(function(){ //c-fadeTrigerというクラス名が
+				var elemPostion = $(this).offset().top-50; //要素より、50px上の
+				var scroll = $(window).scrollTop();
+				var windowHeight = $(window).height();
+				if (scroll >= elemPostion - windowHeight){
+				$(this).addClass('c-fade__in');
+				// 画面内に入ったらfadeInというクラス名を追記
+				}
+				});
+		}
+
+		// $(window).on('load', function(){
+		// 	fadeAnime();/* アニメーション用の関数を呼ぶ*/
+		// });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+	
+		// 画面が読み込まれたらすぐに動かしたい場合の記述
+		$(window).ready(function (){
+			fadeAnime();
+			/* アニメーション用の関数を呼ぶ*/
+		});
+
+
+		// 画面をスクロールをしたら動かしたい場合の記述
+		$(window).scroll(function (){
+			fadeAnime();
+			/* アニメーション用の関数を呼ぶ*/
+		});
+		// ここまで画面をスクロールをしたら動かしたい場合の記述
+	
+	
+	
+
+
+} );
+
---
+++ fade-in/js/swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+  
+  slidesPerView: 1,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+});
